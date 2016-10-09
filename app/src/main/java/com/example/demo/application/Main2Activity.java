11f1471d03be202c80d1582eadcf8212e3e357db
pengfangtao <401384100@qq.com>
2016-10-09 14:07:03 +0800

    呼呼呼呼3

--- app/src/main/java/com/example/demo/application/Main2Activity.java
+++ app/src/main/java/com/example/demo/application/Main2Activity.java
@@ -1,13 +1,37 @@
 package com.example.demo.application;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends Activity implements View.OnClickListener {
+
+    private TextView tv;
+    private Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initView();
+
+    }
+
+    private void initView() {
+        tv = (TextView) findViewById(R.id.tv);
+        bt = (Button) findViewById(R.id.bt);
+
+        bt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt:
+
+                break;
+        }
     }
 }
